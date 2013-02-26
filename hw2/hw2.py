496ee42aba9125d0698b929b9760d61902cde1ed
Ashwath <ashwathpro@gmail.com>
2013-02-26 04:28:02 -0600

    TF IDF created.. but with some errors... check it once again

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -3,24 +3,82 @@ import os
 import re
 from collections import defaultdict
 import cmd
+import json
+from pprint import pprint 
+import math
 
 class HelloWorld(cmd.Cmd):
     """Simple command processor example."""
     
-    index = defaultdict(set)
-    posIndex = defaultdict(lambda:defaultdict(set)) # {word : {1:[1,21,4] }}
-    kgramIndex = defaultdict(set)
+    termFreq = defaultdict(lambda:defaultdict()) # {word : {1:[1,21,4] }}
+    idfList = defaultdict(list)
+    idf = defaultdict()
+    
     
     def do_EOF(self,line):
         return True
     
     def do_test(self,asgbtrh):
-        #if !o: print "!o"
+        if o: print "!o"
             
-    def do_parseFile(self,line):
-	fr=open("mars_tweets_medium.json",'r')
+    def do_parseFile(self,asdfrtg):
+        fr=open('mars_tweets_medium.json','r')
+        allWords = set()
+        lines = fr.readlines()
+        #line = lines[0]
+        numDocs=len(lines)
+        numDocs = 4
+        #for line in lines[0:4]:
+        for i in range(0,3):
+          #print line
+          line= lines[i]
+          json_data=json.loads(line)
+          rawTweetText = json_data['text']
+
+          tweetID = json_data['id']
+          tweetText = re.split('[\W]+',rawTweetText,flags=re.UNICODE)
+          words = tweetText
+          docTermFreq = defaultdict()
+          for word in words:
+            word = word.lower()
+            allWords.add(word)
+            self.idfList[word].append(tweetID)
+            if word in self.idf:
+              self.idf[word] = self.idf[word] + 1 
+            else:
+              self.idf[word] = 1 
+
+
+            if word in docTermFreq:
+              docTermFreq[word] = docTermFreq[word]+1
+            else:
+              docTermFreq[word] = 1
+          
+          self.termFreq[tweetID]=docTermFreq
+
+          #print self.termFreq[tweetID]
+        print len(self.idf)
         
-            
+        print(self.idf['mars'])
+        for doc in self.termFreq: 
+          for word in self.termFreq[doc]:
+            if self.termFreq[doc][word] > 0:
+              self.termFreq[doc][word] = 1 + math.log(self.termFreq[doc][word] ,2)
+            else:
+              self.termFreq[doc][word] = 0
+
+        for word in self.idf:
+          self.idf[word] = math.log(numDocs/self.idf[word],2)
+          print self.idf[word]
+
+        print(self.idf['mars'])
+
+        fr.close()
+
+    def do_query(self,lineadfsggh):
+
+        q=raw_input("Enter a query: ")
+
     def default(self,line):
         print "enter a valid command\n"
     
